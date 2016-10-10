645d730db7f679a4620ee3e87fb99e223d726be8
Ben Jervis <ben@jervis.net.au>
2016-10-11 10:18:49 +1100

    Title and Bootstrap link

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Assignment 7 - Task 1</title>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	</head>
+</html>
+
+
